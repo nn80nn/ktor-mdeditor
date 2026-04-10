2a5789d31db83c3b4cabbbb6d7ec19e6cd2ccd21
nn80nn <147079050+nn80nn@users.noreply.github.com>
2026-04-11 01:45:27 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "ktor-mdeditor"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package n.learn
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
